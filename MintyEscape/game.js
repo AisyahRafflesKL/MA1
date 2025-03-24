39b5663cf30774f58bca1e7d3803cae5f0591880
AisyahGhani <nurulaisyahanaam@gmail.com>
2025-03-25 01:28:30 +0800

    LFS

---
+++ MintyEscape/game.js
@@ -0,0 +1,51 @@
+﻿// Create a global scene for keyboard shortcuts
+class GlobalKeys extends Phaser.Scene {
+    constructor() {
+        super({ key: 'GlobalKeys', active: true });
+    }
+
+    create() {
+        this.input.keyboard.on('keydown-ONE', () => {
+            this.scene.start('world1');
+        });
+
+        this.input.keyboard.on('keydown-TWO', () => {
+            this.scene.start('world2');
+        });
+
+        this.input.keyboard.on('keydown-THREE', () => {
+            this.scene.start('world3');
+        });
+        this.input.keyboard.on('keydown-FOUR', () => {
+            this.scene.start('winScene');
+        });
+    }
+}
+
+var config = {
+    type: Phaser.CANVAS,
+    // pixel size * tile map size * zoom 
+    width: 32 * 30,
+    height: 32 * 25,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: { y: 300 }
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    canvas: {
+        willReadFrequently: true
+    },
+    backgroundColor: '#49AFA5',
+    pixelArt: true,
+    scene: [preloadScene, main, storyboardWMain, world1, storyboardW1, world2, Level2Reminder, 
+        storyboardW2, Level3Reminder, world3, storyboardW3, winScene, GlobalKeys]
+};
+
+var game = new Phaser.Game(config);
+window.excelMint = 0;
---
+++ MintyEscape/preloadScene.js
@@ -0,0 +1,60 @@
+class preloadScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('preloadScene');
+    }
+
+    preload() {
+        this.load.spritesheet("player", "assets/lolaHiker.png", {
+            frameWidth: 40,
+            frameHeight: 58,
+          });
+          this.load.spritesheet("ciggarCanon", "assets/ciggarCanon.png", {
+            frameWidth: 98.07,
+            frameHeight: 70,
+          });
+          this.load.spritesheet("smokeBullet", "assets/smokeBullet.png", {
+            frameWidth: 65.17,
+            frameHeight: 54,
+          });
+          this.load.spritesheet("excelMint", "assets/excelMint.png", {
+            frameWidth: 57.25,
+            frameHeight: 58,
+          });
+          this.load.spritesheet("ciggarPack", "assets/ciggarPack.png", {
+            frameWidth: 57.25,
+            frameHeight: 67,
+          });
+          this.load.spritesheet("controlreminder", "assets/controlreminder.png", {
+            frameWidth: 757.33,
+            frameHeight: 678,
+          });
+    }
+
+    create () {
+        let graphics = this.add.graphics();
+
+        graphics.fillStyle(0xff3300, 1);
+
+        graphics.fillRect(100, 200, 600, 300);
+        graphics.fillRect(100, 100, 100, 100);
+
+        // text
+        this.add.text(120, 110, 'A', { font: '96px Courier', fill: '#000000' });
+        this.add.text(120, 310, 'Press Spacebar to continue yo game', { font: '24px Courier', fill: '#000000' });
+
+        console.log("This is preloadScene spacebar V3");
+
+        // IMPORTANT
+        //this.input.once('pointerdown', function(){ 
+        var spaceDown = this.input.keyboard.addKey('SPACE'); // detect spacebar
+        
+        spaceDown.on('down', function(){ // when pressed
+        console.log("Spacebar pressed, goto gameScene"); // start gameScene
+        this.scene.start("main"); // start gameScene
+        }, this );
+
+    }
+
+}
